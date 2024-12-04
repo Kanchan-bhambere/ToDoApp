9becac0a67a63c442834093ed35b79df74d10281
Kanchan-bhambere <kanchanbhambere1999@gmail.com>
2024-12-04 10:40:41 +0530

    abc yza

--- main.code.py
+++
@@ -1,7 +0,0 @@
-user_prompt = "Enter a ToDo list: "
-ToDo1 = input(user_prompt)
-ToDo2 = input(user_prompt)
-ToDo3 = input(user_prompt)
-
-ToDos = [ToDo1, ToDo2, ToDo3, "completed for Today"]
-print(ToDos)
